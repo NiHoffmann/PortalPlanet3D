bec64353ac56f3c346bb00a800dcb3cda60cf29f
NiHoffmann <82971538+NiHoffmann@users.noreply.github.com>
2022-05-19 19:46:44 +0200

    Portals First Draft
    New:
    Portal Class
    Portal Shader
    Changes:
    small changes to player Controll
    fixed an issue where y coords are added to movement

---
+++ Assets/Portal.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject linkedPortal;
+    public MeshRenderer screen;
+    public string playerName;
+    Camera playerCam;
+    Camera portalCam;
+    RenderTexture viewTexture;
+
+    void Awake()
+    {
+        playerCam = Camera.main;
+        portalCam = GetComponentInChildren<Camera>();
+        portalCam.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+
+        Vector3 position = (linkedPortal.transform.position) + (playerCam.transform.forward.normalized * 1f);
+        //this doesnt work propperly with player
+        position += Vector3.ProjectOnPlane((collision.gameObject.transform.position - this.transform.position), linkedPortal.transform.position.normalized);
+        
+        collision.gameObject.transform.position = position;
+
+    }
+
+    void CreateViewTexture() 
+    {
+
+        if (viewTexture == null || viewTexture.width != Screen.width || viewTexture.height != Screen.height) {
+            if (viewTexture != null) {
+                viewTexture.Release();
+            }
+            viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
+            portalCam.targetTexture = viewTexture;
+            linkedPortal.GetComponent<Portal>().screen.material.SetTexture("_MainTex", viewTexture);
+        }
+    
+    }
+
+    public void Render()
+    {
+        screen.enabled = false;
+
+        portalCam.nearClipPlane = Vector3.Distance(portalCam.transform.position, this.transform.position) - 1f;
+
+        CreateViewTexture();
+
+        var matrix = transform.localToWorldMatrix * linkedPortal.transform.worldToLocalMatrix * playerCam.transform.localToWorldMatrix;
+        portalCam.transform.SetPositionAndRotation(matrix.GetColumn(3), matrix.rotation);
+
+        portalCam.Render();
+    
+        screen.enabled = true;
+    }
+
+    public void Update()
+    {
+        Render();
+    }
+}
