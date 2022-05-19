e03ccf82c46f6ae3e8f17e6717cc14d4c82b108a
NiHoffmann <82971538+NiHoffmann@users.noreply.github.com>
2022-05-19 19:58:17 +0200

    Revert "Revert "Portals First Draft""
    This reverts commit 86b04543a9c6cdf751d88365025d546ca43b2ebc.

---
+++ Assets/GravityGun.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Camera cam;
+    [SerializeField] float maxGrabDist = 10f;
+    [SerializeField] float force = 10f;
+    [SerializeField] Transform objectHolder;
+
+    GameObject grabbedObject = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (grabbedObject && grabbedObject.GetComponent<Rigidbody>())
+        {
+
+            grabbedObject.GetComponent<Rigidbody>().MovePosition(objectHolder.transform.position);
+
+            if (Input.GetMouseButtonDown(0)) {
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                grabbedObject.GetComponent<Rigidbody>().AddForce(cam.transform.forward * force, ForceMode.VelocityChange);
+                //collision with player now enabled
+                Physics.IgnoreCollision(grabbedObject.GetComponent<Collider>(), GetComponent<Collider>(), false);
+
+                grabbedObject = null;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (grabbedObject && grabbedObject.GetComponent<Rigidbody>())
+            {
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                grabbedObject = null;
+            }
+            else
+            {
+                RaycastHit hit;
+                Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+
+                if (Physics.Raycast(ray, out hit, maxGrabDist))
+                {
+                    grabbedObject = hit.collider.gameObject;
+                    if (grabbedObject.GetComponent<Rigidbody>())
+                    {
+                        //ignore collision with player 
+                        Physics.IgnoreCollision(grabbedObject.GetComponent<Collider>(), GetComponent<Collider>(), true);
+                        grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                }
+
+            }
+        }
+    }
+
+}
